229ea32fc49c37e2ada1f74a7f2c1cc5b741584a
qingsuo526 <59449819+qingsuo526@users.noreply.github.com>
2020-01-09 16:03:16 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,11 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' openwrt/package/base-files/files/bin/config_generate
+
+- name: 自定义插件主题。
+        run: |
+          cd package
+          mkdir openwrt-packages
+          cd openwrt-packages
+          git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+          git clone https://github.com/qingsuo526/luci-app-ssr-plus-Jo-1.git
